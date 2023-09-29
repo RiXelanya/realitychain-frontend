3f30a7953e79527e1f3873445b409cc75b47231e
RXRD <lilixinxin@protonmail.com>
2023-09-29 14:10:47 +0700

    add refresh button

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ const App = () => {
   if (typeof window.ethereum === 'undefined') {
     return (
       <div>
-        <p>you dont have metamask wallet</p>
+        <p>you dont have metamask wallet extension installed on your device</p>
       </div>
     )
   }
+  const handleRefresh = () => {
+    window.location.reload()
+  }
+
   return (
     <div className='App'>
       <UserInterface>
 
       </UserInterface>
+      <button className='RefreshButton' onClick={handleRefresh}>Refresh</button>
     </div>
   )
 
